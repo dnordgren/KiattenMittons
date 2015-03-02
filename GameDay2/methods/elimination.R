b1fe6b4d6eda2fbaead8d3b14ba47a64995b6254
NathanDeMaria <naterstd@gmail.com>
2015-03-02 17:14:06 -0600

    Well, it probably helps to be less bad than I am (fixed elimination test)

--- GameDay2/methods/elimination.R
+++ GameDay2/methods/elimination.R
@@ -6,32 +6,7 @@ source('vote_based.R')
 
 elimination_data <- get_data('elimination.csv', rank_flip = T)
 
-get_order <- function(vote_data) {
-	order <- c()
-	# using a for loop because dependent on previous iterations
-	survivors <- copy(vote_data)
-	for(i in 1:(nrow(vote_data) - 1)) {
-		
-		votes_vec <- apply(survivors[,-1,with=F], 2, which.max)
-		votes_table <- table(votes_vec)
-		votes_dt <- data.table(id=names(votes_table), votes=votes_table)
-		zeroes <- (1:nrow(survivors))[!(1:nrow(survivors) %in% votes_dt$id)]
-		
-		if(length(zeroes) > 0) {
-			votes_dt <- rbind(votes_dt, data.table(id=zeroes, votes=0))
-		}
-		
-		# indices of items up for elimination
-		up_for_elim <- as.numeric(votes_dt[which(votes == min(votes)), id])
-		elim_names <- survivors$movie[up_for_elim]
-		
-		# in case of tie, eliminates the last alphabetically
-		chosen <- elim_names[order(elim_names, decreasing = T)][1]
-		order <- c(chosen, order)
-		survivors <- survivors[movie != chosen]
-	}
-	data.table(movie=c(survivors$movie, order))
-}
+source('elimination_helper.R')
 
 get_condorcet(elimination_data)
 get_order(elimination_data)
--- GameDay2/tests/test_elimination.R
+++ GameDay2/tests/test_elimination.R
@@ -1,7 +1,8 @@
 setwd('..')
 
 test_that('Elimination on the example in H15 intro', {
-	dat <- get_data('h15_intro_elimination.csv')
+	dat <- get_data('h15_intro_elimination.csv', T)
+	source('elimination_helper.R', local=T)
 	morder <- get_order(dat)[,movie]
 	expect_equal(morder, c('c', 'a', 'b'))
 })
