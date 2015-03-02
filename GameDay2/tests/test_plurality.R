1cc359f4a0606e5122f0410cdd9fb9212309733a
NathanDeMaria <naterstd@gmail.com>
2015-03-02 16:48:53 -0600

    Source control the test votes

--- GameDay2/tests/test_plurality.R
+++ GameDay2/tests/test_plurality.R
@@ -5,3 +5,9 @@ test_that('Basic plurality', {
 	winner <- get_order(dat)[1,movie]
 	expect_equal(winner, 'Pitch Perfect')
 })
+
+test_that('H15I plurality', {
+	dat <- get_data('h15_intro_plurality.csv')
+	winner <- get_order(dat)[1,movie]
+	expect_equal(winner, 'a')
+})
