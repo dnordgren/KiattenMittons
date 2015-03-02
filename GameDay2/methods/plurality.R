44e4cb39d3994db84bfe194baa649092a1d3aece
NathanDeMaria <naterstd@gmail.com>
2015-03-01 20:50:48 -0600

    Condorcet, I think.

--- GameDay2/methods/plurality.R
+++ GameDay2/methods/plurality.R
@@ -7,3 +7,23 @@ get_order <- function() {
 	plurality_data$votes <- rowSums(plurality_data[,-1,with=F])
 	plurality_data[order(votes, -tolower(movie), decreasing = T), list(movie, votes)]
 }
+
+get_condorcet <- function() {
+	
+	half_prefer <- function(row_num) {
+		others <- (1:nrow(plurality_data))[-row_num]
+		
+		current_row <- plurality_data[row_num, -1, with=F]
+		prefered_over <- sapply(others, function(other) {
+			other_row <- plurality_data[other, -1, with=F]
+			mean(current_row > other_row) >= .5
+		})
+		all(prefered_over)
+	}
+	
+	condorcet <- which(sapply(1:nrow(plurality_data), half_prefer))
+	if(length(condorcet)) {
+		return(plurality_data$movie[condorcet])
+	}
+	return("No condorcet winner")
+}
