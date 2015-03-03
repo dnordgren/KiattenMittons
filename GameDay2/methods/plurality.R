c3c2c60e64938f708900213765e34882a06f08c9
ReesKlintworth <reesk5150@gmail.com>
2015-03-02 23:01:50 -0600

    Save output to excel file

--- GameDay2/methods/plurality.R
+++ GameDay2/methods/plurality.R
@@ -2,7 +2,7 @@
 source('helpers.R')
 source('vote_based.R')
 
-plurality_data <- get_data('plurality_condorcet.csv')
+plurality_data <- get_data('Voting.csv')
 
 get_order(plurality_data)
 get_condorcet(plurality_data)
