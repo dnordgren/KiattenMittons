d0c6334b5d28692c0277a19c0ce4cc98d7dfbf34
NathanDeMaria <naterstd@gmail.com>
2015-04-13 16:56:32 -0500

    Added League with a method for computing total parity

--- FinalProject/Repast/KiattenMittons/src/kiattenMittons/LeagueBuilder.java
+++ FinalProject/Repast/KiattenMittons/src/kiattenMittons/LeagueBuilder.java
@@ -38,6 +38,10 @@ public class LeagueBuilder implements ContextBuilder<Object> {
 		for (Team team: teams) {
 			context.add(team);
 		}
+		
+		League league = new League(teams);
+		context.add(league);
+		
 		return context;
 	}
 
