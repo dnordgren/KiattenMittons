935722c6766c2f27e9a39e1ff3da09641819a65f
NathanDeMaria <naterstd@gmail.com>
2015-04-27 15:49:20 -0500

    Using the batch run data

---
+++ FinalProject/DataGathering/max_player_box.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+source('parity.R')
+
+types <- c('$13.7 mil', '$20.6 mil', '$31.0 mil', '$41.3 mil')
+parity <- get_median_parity('data/experiment3/parity.2015.Apr.27.00_18_14.csv')
+
+parity[,type:=types[ceiling(run/50)]]
+
+g <- ggplot(parity, aes(y=parity, x=type)) + 
+	geom_boxplot() + 
+	labs(title='Player Cap vs Parity', 
+		 y='Parity', 
+		 x='Player Cap') #+ stat_smooth(aes(group=1), method='lm')
+print(g)
--- FinalProject/DataGathering/parity.R
+++ FinalProject/DataGathering/parity.R
@@ -1,16 +1,13 @@
 library(data.table)
+library(readr)
 
 get_median_parity <- function(filename) {
 	parity_data <- data.table(read_csv(filename, col_names=F))
-	setnames(parity_data, c('raw_parity', 'raw_tick'))
+	setnames(parity_data, c('run', 'raw_parity', 'raw_tick'))
 	parity_data[,parity:=as.numeric(as.character(regmatches(raw_parity, 
 															regexpr('[0-9]*\\.[0-9]*', raw_parity))))]
-	parity_data[,mean(parity)]
-}
-
-get_box <- function(dir) {
-	files <- list.files(dir, full.names = T)
-	parity_files <- files[grep('parity', files)]
-	parities <- sapply(parity_files, get_median_parity)
-	data.table(raw_type=dir, parity=parities)
+	parity_data[,run:=regmatches(run, regexpr(' [0-9]*', run))]
+	parity_data[,run:=as.numeric(as.character(run))]
+	parity <- parity_data[,list(parity=mean(parity)),by=run]
+	parity[order(run)]
 }
